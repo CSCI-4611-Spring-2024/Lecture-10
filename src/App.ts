18a530e6c1e8ecc54a124321d296fa0dc92a8e77
Evan Suma Rosenberg <suma@umn.edu>
2024-02-15 16:22:33 -0600

    post-class update

--- src/App.ts
+++ src/App.ts
@@ -4,12 +4,18 @@
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  */ 
 
+// suggest a topic
+// z.umn.edu/4611
+
 import * as gfx from 'gophergfx'
 
 export class App extends gfx.GfxApp
 {
     private ground: gfx.Mesh3;
     private skybox: gfx.Mesh3;
+    private cylinder: gfx.Mesh3;
+
+    private cameraControls: gfx.OrbitControls;
 
     // --- Create the App class ---
     constructor()
@@ -19,6 +25,9 @@ export class App extends gfx.GfxApp
 
         this.ground = gfx.Geometry3Factory.createBox(50, 1, 50);
         this.skybox = gfx.Geometry3Factory.createBox(100, 100, 100);
+        this.cylinder = gfx.Geometry3Factory.createCylinder(20, 1, 1);
+    
+        this.cameraControls = new gfx.OrbitControls(this.camera);
     }
 
 
@@ -28,7 +37,12 @@ export class App extends gfx.GfxApp
         // Setup the camera projection matrix and position.
         // We will learn more about camera models later in this course.
         this.camera.setPerspectiveCamera(60, 1920/1080, 0.1, 100);
-        this.camera.position.set(0, 1.5, 0);
+
+        // Set the camera controls to orbit around the target at a distance
+        // of 4 meters away, with a starting view angle 22 degrees downward
+        // towards the origin
+        this.cameraControls.setDistance(4);
+        this.cameraControls.setOrbit(gfx.MathUtils.degreesToRadians(-22), 0);
 
         // Create an ambient light that illuminates everything in the scene
         const ambientLight = new gfx.AmbientLight(new gfx.Color(0.4, 0.4, 0.4));
@@ -37,8 +51,11 @@ export class App extends gfx.GfxApp
         const directionalLight = new gfx.DirectionalLight(new gfx.Color(0.6, 0.6, 0.6));
         directionalLight.position.set(1, 2, 1);
 
-        this.ground.position.set(0, -.5, 0);
+        this.ground.position.set(0, -1, 0);
         this.ground.material.setColor(new gfx.Color(83/255, 209/255, 110/255));
+
+        const cylinderMaterial = new gfx.GouraudMaterial();
+        this.cylinder.material = cylinderMaterial;
 
         this.skybox.material = new gfx.UnlitMaterial();
         this.skybox.material.side = gfx.Side.BACK;
@@ -48,12 +65,13 @@ export class App extends gfx.GfxApp
         this.scene.add(directionalLight);
         this.scene.add(this.ground);
         this.scene.add(this.skybox);
+        this.scene.add(this.cylinder);
     }
 
     
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
-        
+        this.cameraControls.update(deltaTime);
     }
 }
